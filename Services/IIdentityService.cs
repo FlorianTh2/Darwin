96a009cc957990ad6dc1847efa3c9d2836da7873
unknown <thom.florian.1997@googlemail.com>
2021-12-31 02:27:41 +0100

    Minor changes

--- Services/IIdentityService.cs
+++ Services/IIdentityService.cs
@@ -4,6 +4,8 @@ namespace hello_asp_identity.Services;
 
 public interface IIdentityService
 {
+    // weitere methodensignaturen hinzufügen: im grunde 1 foreach controller-endpoint
+
     Task<AuthenticationResult> RegisterAsync(string username, string email, string password);
     Task<AuthenticationResult> LoginAsync(string username, string password);
     Task<AuthenticationResult> RefreshTokenAsync(string token, string refreshToken);
