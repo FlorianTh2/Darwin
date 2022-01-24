40a17225b8ff1a4b0f51a2788b04a4f24df01004
unknown <thom.florian.1997@googlemail.com>
2022-01-24 23:24:15 +0100

    Minor changes

--- Services/IdentityService.cs
+++ Services/IdentityService.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Text;
 using System.Web;
 using hello_asp_identity.Contracts.V1;
-using hello_asp_identity.Contracts.V1.Responses;
 using hello_asp_identity.Data;
 using hello_asp_identity.Domain;
 using hello_asp_identity.Domain.Enums;
